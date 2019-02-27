138a9170ba8a114a1588c7a5c79f241edde3a9fd
HubHermit <HubHermit@users.noreply.github.com>
2019-02-26 21:00:10 -0400

    I think I got it this time

---
+++ src/rdna.hpp
@@ -0,0 +1,33 @@
+#include <random>
+#include <string>
+
+
+using std::string;
+
+
+string randDNA (int seed, string bases, int n)
+{
+	string dnaSEQ(bases);//for reference to see how this works	
+		
+		
+	
+	
+	int min = 0;//min number
+	int max = bases.size()-1;//max number	
+
+	
+	std::mt19937 gen(seed); //using a Mersenne Twister generator for the seed
+	std::uniform_int_distribution <int> dist(min, max);
+
+	        
+			
+        
+	for (int v =0; v < n; v ++) 
+	{
+		int shuffle = dist(gen);
+		dnaSEQ += bases[shuffle];
+		
+	}
+
+return dnaSEQ;
+}
